9e8eac3b0e755e02602e7ba29438f817470f042c
Sergey <Sergey@localhost.localdomain>
2023-02-23 22:52:43 +0300

    Add filter for only restaurants able prepare order

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.shortcuts import reverse, redirect
+from django.db.models import Prefetch
 from django.templatetags.static import static
 from django.utils.html import format_html
 from django.utils.http import url_has_allowed_host_and_scheme
@@ -135,6 +136,18 @@ class OrderAdmin(admin.ModelAdmin):
                 instance.price = instance.product.price
             instance.save()
         formset.save_m2m()
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+
+        order_components = obj.components.select_related('product')
+        order_products = [component.product for component in order_components]
+        order_menu_items = RestaurantMenuItem.objects.filter(product__in=order_products).select_related('restaurant')
+
+        restaurants_ids = [menu_item.restaurant.id for menu_item in order_menu_items]
+        restaurant_queryset = Restaurant.objects.filter(id__in=restaurants_ids)
+        form.base_fields['restaurant'].queryset = restaurant_queryset
+        return form
 
     def response_change(self, request, obj):
         response = super().response_change(request, obj)
