2947e3672456de59d331e6c38bb5c362c674b964
Sergey <Sergey@localhost.localdomain>
2023-02-26 04:58:04 +0300

    Create new app for adresses joined with geocoordinates

---
+++ mappoint/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class MapPoint(models.Model):
+    address = models.CharField(
+        'адрес',
+        max_length=100,
+    )
+    latitude = models.FloatField(
+        verbose_name='Широта',
+        blank=True,
+        null=True
+    )
+    longitude = models.FloatField(
+        verbose_name='Долгота',
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = 'место на карте'
+        verbose_name_plural = 'места на карте'
+
+    def __str__(self):
+        return self.address
