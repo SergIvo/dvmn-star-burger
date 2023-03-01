552c0cddb3695788f70c0fda46c3ea352f3d8074
Sergey <Sergey@localhost.localdomain>
2023-03-01 23:05:19 +0300

    Change verbose names for datetime fields at order model

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -185,18 +185,18 @@ class Order(models.Model):
         blank=True,
     )
     creation_dateitme = models.DateTimeField(
-        'создан',
+        'дата и время создания',
         default=timezone.now,
         db_index=True
     )
     confirmation_dateitme = models.DateTimeField(
-        'Подтвержден',
+        'дата и время подтверждения',
         blank=True,
         null=True,
         db_index=True
     )
     delivery_dateitme = models.DateTimeField(
-        'доставлен',
+        'дата и время доставки',
         blank=True,
         null=True,
         db_index=True
