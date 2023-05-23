5706b54c8825a65d43e93c561b2e9b112803dab8
Sergey <Sergey@localhost.localdomain>
2023-05-24 01:14:23 +0300

    Deployment script reports to Rollbar

--- deploy_starburger.sh
+++ deploy_starburger.sh
@@ -29,4 +29,24 @@ python3 manage.py migrate
 
 systemctl restart starburger-django
 
+TOKEN=$(cat .env | grep ROLLBAR_ACCESS_TOKEN | cut -d'=' -f 2)
+ROLLBARENV=$(cat .env | grep ROLLBAR_ENVIRONMENT | cut -d'=' -f 2)
+COMMIT=$(git rev-parse HEAD)
+COMMENT=$(git log -1 --pretty=format:%B)
+
+JSON=$(jq -n \
+--arg env "$ROLLBARENV" \
+--arg hash "$COMMIT" \
+--arg name "$(whoami)" \
+--arg comment "$COMMENT" \
+'{"environment": $env, "revision": $hash, "local_username": $name, "comment": $comment}'
+)
+
+curl --request POST \
+     --url https://api.rollbar.com/api/1/deploy \
+     --header 'accept: application/json' \
+     --header 'content-type: application/json' \
+     --header 'X-Rollbar-Access-Token: '$TOKEN \
+     --data "$JSON"
+
 echo "Deployed successfully"
