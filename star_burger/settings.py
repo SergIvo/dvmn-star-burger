ae2a49cc073303a8f8ec43a7e70ee67b778ff6c8
Sergey <Sergey@localhost.localdomain>
2023-07-17 01:34:03 +0300

    Add nginx proxy name to allowed hosts

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -16,6 +16,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS.append('django')
 
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
